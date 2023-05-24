2623d95c99c977495dece581b68a19f482c72e62
danknox1977 <127804873+danknox1977@users.noreply.github.com>
2023-05-24 16:18:07 -0400

    Basic working routers complete

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,125 +1,78 @@
-const { blog } = require('../api/blog.json')
+//required modules, file system, express 
 
+const fs = require('fs');
+const express = require('express');
+const router = express.Router();
 
-const express = require('express')
-const router = express.Router()
+// imports blog.JSON file and turns into a js friendly format
+const jsonData = fs.readFileSync('./api/blog.json', 'utf8');
+let blogData = JSON.parse(jsonData);
 
+// handler to get all the blog posts
+router.get('/blogs', (req, res) => {
+  res.json(blogData);
+});
 
+// Get a specific blog by post_id
+router.get('/blogs/:post_id', (req, res) => {
+  const { post_id } = req.params;
+  const blog = blogData.find((blog) => blog.post_id === parseInt(post_id));
 
-
-// express waits until it gets a GET request at the / endpoint
-// when it gets that request, it creates a req and res object
-// THEN it calls handler and passes it those two objects
-// (handler is a function that takes in those objects as arguments)
-
-
-router.get('/all', (req, res) => {
-  res.json(blog)
-
-})
-
-router.get('/:id', (req, res) => {
-  // find appropriate post
-  const post = post_id.find(post => post.id === req.params.id)
-
-  // send it back to the user
-  res.json(post)
-})
-
-router.get('/author/:userId', (req, res) => {
-  const postsFiltered = posts.filter(
-    post => post.userId === parseInt(req.params.userId)
-  )
-  res.json(postsFiltered)
-})
-
-router.post('/new', (req, res) => {
-  // db.sort((a, b) => parseInt(a.id) < parseInt(b.id))
-  // console.log(db)
-  // console.log(parseInt(db.at(-1).id) + 1)
-
-  const ids = blog.map(p => parseInt(p.id))
-  let newId = Math.max(...ids) + 1
-  newId = newId.toString().padStart(3, '0')
-
-  req.body.id = newId
-
-  blog.push(req.body)
-
-  res.send('added new post')
-})
-
-
-router.put('/:id', (req, res) => {
-  let postIndex = blog.findIndex(
-    post => parseInt(post.id) === parseInt(req.params.id)
-  )
-
-  const id = blog[postIndex].id
-
-
-  db[postIndex] = req.body
-  db[postIndex].id = id
-
-  res.json({
-    message: 'update successful',
-    recordUpdated: req.body
-  })
-})
-
-// write the code for a PATCH endpoint at '/:id'
-// router.patch('/:id', (req, res) => {
-//   // your code goes here!
-// let postIndex = blog.findIndex(
-//     post => parseInt(post.id) === parseInt(req.params.id)
-//   )
-// const id = blog[postIndex].id
-
-//   db[postIndex] = req.authorId
-
-//   db[postIndex] = req.task
-
-//   db[postIndex] = req.completed
-//   }
-
-
-//example 
-router.patch('/:id', (req, res) => {
-  const blogId = parseInt(req.params.id);
-  const blogPatch = blog.find((blog) => blog.id === blogId);
-  if (blogPatch) {
-    if (req.body.task) {
-      blogPatch.task = req.body.task;
-    }
-    if (req.body.authorId) {
-      blogPatch.authorId = req.body.authorId;
-    }
-    if (req.body.completed !== undefined) {
-      blogPatch.completed = req.body.completed;
-    }
-    res.json(blogPatch);
+  if (blog) {
+    res.json(blog);
   } else {
-    res.status(404).json({ error: 'Task not found' });
+    res.status(404).json({ error: 'The blog you entered does not exist' });
   }
 });
 
+// Post a new blog
+router.post('/blogs', (req, res) => {
+  const { title, author, body } = req.body;
+  const newBlog = { post_id: blogData.length + 1, title, author, body };
 
+  blogData.push(newBlog);
+  updateBlog();
 
+  res.status(201).json(newBlog);
+});
 
-router.delete('/:id', (req, res) => {
-  // this code has to actually do the deleting
+// Replace a specific blog by post_id
+router.put('/blogs/:post_id', (req, res) => {
+  const { post_id } = req.params;
+  const { title, author, body } = req.body;
+  const blog = blogData.find((blog) => blog.post_id === parseInt(post_id));
 
-  const index = blog.findIndex(
-    post => parseInt(post.id) === parseInt(req.params.id)
-  )
-  const deleted = blog.splice(index, 1)
+  if (blog) {
+    blog.title = title || blog.title;
+    blog.author = author || blog.author;
+    blog.body = body || blog.body;
+    updateBlog();
 
-  res.json({
-    message: 'deletion successful',
-    recordsDeleted: deleted
-  })
-})
+    res.json(blog);
+  } else {
+    res.status(404).json({ error: 'The blog you entered does not exist' });
+  }
+});
 
+// Delete a specific blog by post_id
+router.delete('/blogs/:post_id', (req, res) => {
+  const { post_id } = req.params;
+  const index = blogData.findIndex((blog) => blog.post_id === parseInt(post_id));
 
+  if (index !== -1) {
+    const deletedBlog = blogData.splice(index, 1)[0];
+    updateBlog();
 
-module.exports = router
+    res.json(deletedBlog);
+  } else {
+    res.status(404).json({ error: 'The blog you entered does not exist' });
+  }
+});
+
+// Update blog.json
+function updateBlog() {
+  const updatedData = JSON.stringify(blogData, null, 2);
+  fs.writeFileSync('./api/blog.json', updatedData, 'utf8');
+}
+
+module.exports = router;
